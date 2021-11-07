d4fdf35e6192c581450d6c5ae0a542396b33bbc3
SomaSekimoto <0421.soma@gmail.com>
2021-11-07 18:46:44 +0900

    fix: deleteTodo & feat: filterTodod

--- pages/todo.vue
+++ pages/todo.vue
@@ -9,13 +9,15 @@
         </v-form>
       </v-col>
       <v-col cols="12" xs="12" sm="12" md="6" lg="6" xl="6">
+        <v-text-field @input="filterTodos(keyword.keyword)" v-model="keyword.keyword"></v-text-field>
         <v-list>
           <v-list-item v-for="(item, index) in todos" :key="index">
             <v-list-item-content>
               <v-list-item-title>{{ item.name }}</v-list-item-title>
               <v-list-item-subtitle>{{ item.description }}</v-list-item-subtitle>
+              <p>version: {{item._version}}</p>
             </v-list-item-content>
-            <v-btn dark @click="remove(item.id)">削除</v-btn>
+            <v-btn dark @click="remove(item.id, item._version)">削除</v-btn>
           </v-list-item>
         </v-list>
       </v-col>
@@ -28,11 +30,14 @@ import {
   defineComponent,
   computed,
   ref,
+  reactive,
   onMounted
 } from "@vue/composition-api";
 import { createTodo, deleteTodo } from "~/src/graphql/mutations";
-import { API } from "aws-amplify";
-import { listTodos } from "~/src/graphql/queries";
+import { API, graphqlOperation } from "aws-amplify";
+import { listTodos, syncTodos } from "~/src/graphql/queries";
+import { Todo } from "../src/models";
+import { DataStore } from "@aws-amplify/datastore";
 
 type Todo = {
   name: string;
@@ -41,6 +46,7 @@ type Todo = {
 
 type TodoForDel = {
   id: number;
+  _version: number;
 };
 
 const createTodoDtoDefaults: Todo = Object.freeze({
@@ -49,7 +55,8 @@ const createTodoDtoDefaults: Todo = Object.freeze({
 });
 
 const deleteTodoDtoDefaults: TodoForDel = Object.freeze({
-  id: 0
+  id: 0,
+  _version: null
 });
 
 export default defineComponent({
@@ -64,6 +71,11 @@ export default defineComponent({
 
     const formRef = ref();
 
+    // const keyword = ref();
+    const keyword = reactive<{ keyword: string }>({
+      keyword: ""
+    });
+
     const requiredRule = computed(() => [
       (v: string) => !!v || "This field is required"
     ]);
@@ -72,6 +84,23 @@ export default defineComponent({
       const response = await API.graphql({
         query: listTodos
       });
+
+      // @ts-ignore
+      todos.value = response.data.listTodos.items as Todo[];
+    }
+
+    async function filterTodos(keyword: string) {
+      if (keyword == "") return fetchAllTodo();
+
+      const response = await API.graphql(
+        graphqlOperation(listTodos, {
+          filter: {
+            description: {
+              contains: keyword
+            }
+          }
+        })
+      );
       // @ts-ignore
       todos.value = response.data.listTodos.items as Todo[];
     }
@@ -84,6 +113,8 @@ export default defineComponent({
             input: formData.value
           }
         });
+        // console.log("datastoring");
+        // await DataStore.save(new Todo(formData.value));
 
         // reset the `formData` values
         formData.value = {
@@ -93,8 +124,11 @@ export default defineComponent({
       }
     }
 
-    async function remove(id: number) {
+    async function remove(id: number, version: number) {
+      console.log("id");
+      console.log(id);
       formDataForDel.value.id = id;
+      formDataForDel.value._version = version;
       await API.graphql({
         query: deleteTodo,
         variables: {
@@ -118,6 +152,8 @@ export default defineComponent({
       formRef,
       submit,
       remove,
+      filterTodos,
+      keyword,
       requiredRule
     };
   }
